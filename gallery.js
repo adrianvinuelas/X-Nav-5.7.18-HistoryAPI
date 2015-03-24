191a2d06d6250fceb629bd53019c5190b80614d5
adrianvinuelas <adrian.vinuelas@gmail.com>
2015-03-24 14:51:50 +0100

    falta por hacer gh pages

--- gallery.js
+++ gallery.js
@@ -1,11 +1,15 @@
 function supports_history_api() {
-  return !!(window.history && history.pushState);
+  if(Modernizr.history){
+	    return true;
+    }else{
+        return false;
+    }
 }
 
 function swapPhoto(href) {
   var req = new XMLHttpRequest();
   req.open("GET",
-           "http://gsyc.es/~grex/history_api/gallery/" +
+           "http://localhost:1234/gallery/" +
              href.split("/").pop(),
            false);
   req.send(null);
