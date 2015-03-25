3be36c42df93c6b4f4347e79b3acb9acbe37cf0d
adrianvinuelas <adrian.vinuelas@gmail.com>
2015-03-25 16:56:05 +0100

    haciendo pruebas para ver si funciona en gh-pages

--- gallery.js
+++ gallery.js
@@ -9,7 +9,7 @@ function supports_history_api() {
 function swapPhoto(href) {
   var req = new XMLHttpRequest();
   req.open("GET",
-           "http://localhost:1234/gallery/" +
+           "https://github.com/adrianvinuelas/X-Nav-5.7.18-HistoryAPI/tree/master/gallery/" +
              href.split("/").pop(),
            false);
   req.send(null);
